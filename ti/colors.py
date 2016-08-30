f2c2cb18d754f6a12f6b2d3848f39a49501adada
Eduard Kracmar <eduard@adaptiware.com>
2016-08-30 13:17:34 +0200

    Colors fixes, default is True

--- ti/colors.py
+++ ti/colors.py
@@ -2,28 +2,28 @@
 from colorama import Fore
 
 
-def red(_str, use_color=False):
+def red(_str, use_color=True):
     if use_color:
         return Fore.RED + _str + Fore.RESET
     else:
         return _str
 
 
-def green(_str, use_color=False):
+def green(_str, use_color=True):
     if use_color:
         return Fore.GREEN + _str + Fore.RESET
     else:
         return _str
 
 
-def yellow(_str, use_color=False):
+def yellow(_str, use_color=True):
     if use_color:
         return Fore.YELLOW + _str + Fore.RESET
     else:
         return _str
 
 
-def blue(_str, use_color=False):
+def blue(_str, use_color=True):
     if use_color:
         return Fore.BLUE + _str + Fore.RESET
     else:
